489b69cf87e65db1779cdeb6623ff94b3ca95364
Quang Huy <quanghuyhgkma@gmail.com>
2023-06-22 17:00:37 +0700

    Push code details
    Up code details warehouse

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,7 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
    * Fires the scrollto function on click to links .scrollto
    */
   let selectScrollto = document.querySelectorAll('.scrollto');
-  selectScrollto.forEach(el => el.addEventListener('click', function(event) {
+  selectScrollto.forEach(el => el.addEventListener('click', function (event) {
     if (document.querySelector(this.hash)) {
       event.preventDefault();
 
@@ -108,7 +108,7 @@ document.addEventListener('DOMContentLoaded', () => {
    */
   const mobileNavToogle = document.querySelector('.mobile-nav-toggle');
   if (mobileNavToogle) {
-    mobileNavToogle.addEventListener('click', function(event) {
+    mobileNavToogle.addEventListener('click', function (event) {
       event.preventDefault();
 
       document.querySelector('body').classList.toggle('mobile-nav-active');
@@ -124,7 +124,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
 
   navDropdowns.forEach(el => {
-    el.addEventListener('click', function(event) {
+    el.addEventListener('click', function (event) {
       if (document.querySelector('.mobile-nav-active')) {
         event.preventDefault();
         this.classList.toggle('active');
@@ -158,7 +158,7 @@ document.addEventListener('DOMContentLoaded', () => {
    */
   const scrollTop = document.querySelector('.scroll-top');
   if (scrollTop) {
-    const togglescrollTop = function() {
+    const togglescrollTop = function () {
       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
     }
     window.addEventListener('load', togglescrollTop);
@@ -196,8 +196,8 @@ document.addEventListener('DOMContentLoaded', () => {
       });
 
       let menuFilters = document.querySelectorAll('.portfolio-isotope .portfolio-flters li');
-      menuFilters.forEach(function(el) {
-        el.addEventListener('click', function() {
+      menuFilters.forEach(function (el) {
+        el.addEventListener('click', function () {
           document.querySelector('.portfolio-isotope .portfolio-flters .filter-active').classList.remove('filter-active');
           this.classList.add('filter-active');
           portfolioIsotope.arrange({
@@ -296,3 +296,4 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
